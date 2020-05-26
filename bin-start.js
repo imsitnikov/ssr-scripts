cc76b4f87f2e0d910758da084b5dbe42c481648f
Sitnikov Maksim <sitnikov.maksim@wildberries.ru>
2020-05-26 15:35:30 +0300

    Add bin-start.js

---
+++ bin-start.js
@@ -0,0 +1,50 @@
+const { execSync } = require('child_process');
+
+const args = process.argv.slice(2);
+
+const scriptIndex = args.findIndex(
+  x => x === 'start' || x === 'build'
+);
+
+const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
+// const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
+
+if (['start', 'build'].includes(script)) {
+  const runPath = require.resolve('./scripts/run');
+  const babelNodeConfigPath = require.resolve('./configs/babel-node.config.js');
+
+  switch (script) {
+    case 'start':
+      execSync('babel-node ' + runPath + ' start --config-file ' + babelNodeConfigPath + ' --silent --inspect');
+      break;
+    case 'build':
+      execSync('babel-node ' + runPath + ' build --config-file ' + babelNodeConfigPath);
+      break;
+  }
+  // const result = spawn.sync(
+  //   'node',
+  //   nodeArgs
+  //     .concat(require.resolve('../scripts/' + script))
+  //     .concat(args.slice(scriptIndex + 1)),
+  //   { stdio: 'inherit' }
+  // );
+  // if (result.signal) {
+  //   if (result.signal === 'SIGKILL') {
+  //     console.log(
+  //       'The build failed because the process exited too early. ' +
+  //         'This probably means the system ran out of memory or someone called ' +
+  //         '`kill -9` on the process.'
+  //     );
+  //   } else if (result.signal === 'SIGTERM') {
+  //     console.log(
+  //       'The build failed because the process exited too early. ' +
+  //         'Someone might have called `kill` or `killall`, or the system could ' +
+  //         'be shutting down.'
+  //     );
+  //   }
+  //   process.exit(1);
+  // }
+  process.exit(1);
+} else {
+  console.log('Unknown script "' + script + '".');
+}
